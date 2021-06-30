a1ff8dc0319eedb9cf178781b9049c848b00ad65
ahamedsahib <adsahib39@gmail.com>
2021-06-30 09:02:22 +0530

    [Ahamed]Adding Dice rolled Number

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -9,6 +9,11 @@ namespace SnakeAndLadder
             Console.WriteLine("Welcome to snake and ladder simulator");
             int position = 0;
             Console.WriteLine($"player start with position {position}");
+            Random random = new Random();
+            int rolled_dice = random.Next(1, 7);
+            Console.WriteLine($"The Dice rolled as {rolled_dice}");
+
+
         }
     }
 }
