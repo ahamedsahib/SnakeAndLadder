173dbaa1fb6478b58c546cb048bcade16bf25ff2
ahamedsahib <adsahib39@gmail.com>
2021-06-30 10:20:19 +0530

    [Ahamed]picking up options to continue game

---
+++ SnakeAndLadder/GameSimulator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class GameSimulator
+    {
+        public int position = 0;
+        public const int noPlay = 1;
+        public const int ladder = 2;
+        public const int snake = 3;
+        public void Game()
+        {
+            
+            Random random = new Random();
+            int rolled_dice = random.Next(1, 7);
+            int option = random.Next(1, 4);
+            Console.WriteLine($"The Dice rolled as {rolled_dice}");
+            if (option == 2)
+            {
+                Console.WriteLine("Player got Ladder and Climbing up");
+                position +=  rolled_dice;
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine("Player hit by a snake");
+                position -= rolled_dice;
+            }
+            else
+            {
+                Console.WriteLine("player had no chance to play ");
+            }
+
+            Console.WriteLine($"Player position is {position} ");
+
+        }
+
+
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,12 +7,8 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snake and ladder simulator");
-            int position = 0;
-            Console.WriteLine($"player start with position {position}");
-            Random random = new Random();
-            int rolled_dice = random.Next(1, 7);
-            Console.WriteLine($"The Dice rolled as {rolled_dice}");
-
+            GameSimulator game = new GameSimulator();
+            game.Game();
 
         }
     }
